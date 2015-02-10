746aa9245ec8b9a636a821a2772753b282adedc9
Targuinus <rens.klaren@gmail.com>
2015-02-10 16:40:31 +0100

    Updated game a bit
    Had to remove the prestige system because it did not work.

--- script.js
+++ script.js
@@ -1,208 +1,200 @@
 // Game config
-var buildings;
-var upgrades;
-function setUpgrades() {
-    upgrades = [
-        {
-            cost: 10,
-            powerPerClick: 0.3
+var upgrades = [
+    {
+        cost: 10,
+        powerPerClick: 0.3
+    },
+    {
+        cost: 30,
+        powerPerClick: 0.6
+    },
+    {
+        cost: 90,
+        powerPerClick: 1.2
+    },
+    {
+        cost: 270,
+        powerPerClick: 2.4
+    },
+    {
+        cost: 810,
+        powerPerClick: 4.8
+    },
+    {
+        cost: 2430,
+        powerPerClick: 9.6
+    }
+];
+
+var buildings = [
+    {
+        cost: 1000,
+        amount: 0,
+        upgrades: {
+            amount: 0,
+            cost: 1000000,
+            upgrades: {
+                amount: 0,
+                cost: 1000000000
+            }
         },
-        {
-            cost: 30,
-            powerPerClick: 0.6
-        },
-        {
-            cost: 90,
-            powerPerClick: 1.2
-        },
-        {
-            cost: 270,
-            powerPerClick: 2.4
-        },
-        {
-            cost: 810,
-            powerPerClick: 4.8
-        },
-        {
-            cost: 2430,
-            powerPerClick: 9.6
+        action: {
+            type: "score",
+            perSecond: 10
         }
-    ];
-}
-function setBuildings() {
-    buildings = [
-        {
-            cost: 1000,
+    },
+    {
+        cost: 100000,
+        amount: 0,
+        upgrades: {
             amount: 0,
+            cost: 100000000,
             upgrades: {
                 amount: 0,
-                cost: 1000000,
-                upgrades: {
-                    amount: 0,
-                    cost: 1000000000
-                }
-            },
-            action: {
-                type: "score",
-                perSecond: 10
+                cost: 100000000000
             }
         },
-        {
-            cost: 100000,
-            amount: 0,
-            upgrades: {
-                amount: 0,
-                cost: 100000000,
-                upgrades: {
-                    amount: 0,
-                    cost: 100000000000
-                }
-            },
-            action: {
-                type: "building",
-                building: 0,
-                perSecond: 1
-            }
-        },
-        {
-            cost: 10000000,
-            amount: 0,
-            upgrades: {
-                amount: 0,
-                cost: 10000000000,
-                upgrades: {
-                    amount: 0,
-                    cost: 10000000000000
-                }
-            },
-            action: {
-                type: "building",
-                building: 1,
-                perSecond: 1
-            }
-        },
-        {
-            cost: 1000000000,
-            amount: 0,
-            upgrades: {
-                amount: 0,
-                cost: 1000000000000,
-                upgrades: {
-                    amount: 0,
-                    cost: 1000000000000000
-                }
-            },
-            action: {
-                type: "building",
-                building: 2,
-                perSecond: 1
-            }
-        },
-        {
-            cost: 100000000000,
-            amount: 0,
-            upgrades: {
-                amount: 0,
-                cost: 100000000000000,
-                upgrades: {
-                    amount: 0,
-                    cost: 100000000000000000
-                }
-            },
-            action: {
-                type: "building",
-                building: 3,
-                perSecond: 1
-            }
-        },
-        {
-            cost: 10000000000000,
-            amount: 0,
-            upgrades: {
-                amount: 0,
-                cost: 10000000000000000,
-                upgrades: {
-                    amount: 0,
-                    cost: 10000000000000000000
-                }
-            },
-            action: {
-                type: "building",
-                building: 4,
-                perSecond: 1
-            }
+        action: {
+            type: "building",
+            building: 0,
+            perSecond: 1
         }
-    ];
-}
-setUpgrades();
-setBuildings();
+    },
+    {
+        cost: 10000000,
+        amount: 0,
+        upgrades: {
+            amount: 0,
+            cost: 10000000000,
+            upgrades: {
+                amount: 0,
+                cost: 10000000000000
+            }
+        },
+        action: {
+            type: "building",
+            building: 1,
+            perSecond: 1
+        }
+    },
+    {
+        cost: 1000000000,
+        amount: 0,
+        upgrades: {
+            amount: 0,
+            cost: 1000000000000,
+            upgrades: {
+                amount: 0,
+                cost: 1000000000000000
+            }
+        },
+        action: {
+            type: "building",
+            building: 2,
+            perSecond: 1
+        }
+    },
+    {
+        cost: 100000000000,
+        amount: 0,
+        upgrades: {
+            amount: 0,
+            cost: 100000000000000,
+            upgrades: {
+                amount: 0,
+                cost: 100000000000000000
+            }
+        },
+        action: {
+            type: "building",
+            building: 3,
+            perSecond: 1
+        }
+    },
+    {
+        cost: 10000000000000,
+        amount: 0,
+        upgrades: {
+            amount: 0,
+            cost: 10000000000000000,
+            upgrades: {
+                amount: 0,
+                cost: 10000000000000000000
+            }
+        },
+        action: {
+            type: "building",
+            building: 4,
+            perSecond: 1
+        }
+    }
+];
 
 // Elements
 var elements = {
-	incrementButton: document.querySelector(".js-increment-button"),
-    prestigeButton: document.querySelector(".js-prestige-button"),
+    incrementButton: document.querySelector(".js-increment-button"),
+//    prestigeButton: document.querySelector(".js-prestige-button"),
 //    saveButton: document.querySelector(".js-save-button"),
 //    deleteSaveButton: document.querySelector(".js-delete-save-button"),
-	upgradesContainer: document.querySelector(".js-upgrades"),
-	buildingsContainer: document.querySelector(".js-buildings"),
-	buildingUpgradesContainer: document.querySelector(".js-building-upgrades"),
-	buildingUpgrades2Container: document.querySelector(".js-building-upgrades-second"),
-	score: document.querySelector(".js-info .js-score"),
-	perSecond: document.querySelector(".js-info .js-score-per-second"),
-	powerPerClick: document.querySelector(".js-info .js-power-per-click"),
-	prestige: document.querySelector(".js-prestige"),
-	prestigeNext: document.querySelector(".js-next-prestige")
+    upgradesContainer: document.querySelector(".js-upgrades"),
+    buildingsContainer: document.querySelector(".js-buildings"),
+    buildingUpgradesContainer: document.querySelector(".js-building-upgrades"),
+    buildingUpgrades2Container: document.querySelector(".js-building-upgrades-second"),
+    score: document.querySelector(".js-info .js-score"),
+    perSecond: document.querySelector(".js-info .js-score-per-second"),
+    powerPerClick: document.querySelector(".js-info .js-power-per-click"),
+//    prestige: document.querySelector(".js-prestige"),
+//    prestigeNext: document.querySelector(".js-next-prestige")
 };
 
 for (var i = 0; i < upgrades.length; i++) {
-	var upgrade = upgrades[i];
-	var upgradeElement = document.createElement("button");
-	upgrade.element = upgradeElement;
-	
-	upgradeElement.className = "button";
-	upgradeElement.innerHTML = "+<span class='js-power-per-click'>" + upgrade.powerPerClick + "</span>/s per click<br />" + "Cost: <span class='js-cost'>" + upgrade.cost + "</span>";
-	
-	elements.upgradesContainer.appendChild(upgradeElement);
+    var upgrade = upgrades[i];
+    var upgradeElement = document.createElement("button");
+    upgrade.element = upgradeElement;
+
+    upgradeElement.className = "button";
+    upgradeElement.innerHTML = "+<span class='js-power-per-click'>" + upgrade.powerPerClick + "</span>/s per click<br />" + "Cost: <span class='js-cost'>" + upgrade.cost + "</span>";
+
+    elements.upgradesContainer.appendChild(upgradeElement);
 }
 
 for (var i = 0; i < buildings.length; i++) {
     var building = buildings[i];
     var buildingUpgrade = buildings[i].upgrades;
     var buildingUpgrade2 = buildings[i].upgrades.upgrades;
-	var buildingElement = document.createElement("button");
+    var buildingElement = document.createElement("button");
     var buildingUpgradeElement = document.createElement("button");
     var buildingUpgrade2Element = document.createElement("button");
-	building.element = buildingElement;
+    building.element = buildingElement;
     buildingUpgrade.element = buildingUpgradeElement;
     buildingUpgrade2.element = buildingUpgrade2Element;
-	
-	var html;
+
+    var html;
     var html2;
     var html3;
-	
-	switch (building.action.type) {
-		case "score":
-			html = "+<span class='js-score-per-second'>" + building.action.perSecond + "</span> score per second<br />";
-			break;
-		
-		case "building":
-			html = "+<span class='js-building-per-second'>" + building.action.perSecond + "</span> buildings per second<br />";
-			break;
+
+    switch (building.action.type) {
+        case "score":
+            html = "+<span class='js-score-per-second'>" + building.action.perSecond + "</span> score per second<br />";
+            break;
+
+        case "building":
+            html = "+<span class='js-building-per-second'>" + building.action.perSecond + "</span> buildings per second<br />";
+            break;
     }
-	
+
     html += "Cost: <span class='js-cost'>" + building.cost + "</span><br />Amount: <span class='js-amount'>" + building.amount + "</span>";
     html2 = "+1 multiplier<br />" + "Cost: <span class='js-cost'>" + buildingUpgrade.cost + "</span><br />" + "Amount: <span class='js-amount'>" + buildingUpgrade.amount + "</span><br />";
     html3 = "+1 multiplier<br />" + "Cost: <span class='js-cost'>" + buildingUpgrade2.cost + "</span><br />" + "Amount: <span class='js-amount'>" + buildingUpgrade2.amount + "</span><br />";
-    
-	buildingElement.className = "button";
-	buildingElement.innerHTML = html;
+
+    buildingElement.className = "button";
+    buildingElement.innerHTML = html;
     buildingUpgradeElement.className = "button";
     buildingUpgradeElement.innerHTML = html2;
     buildingUpgrade2Element.className = "button";
     buildingUpgrade2Element.innerHTML = html3;
-    
-	
-	elements.buildingsContainer.appendChild(buildingElement);
+
+    elements.buildingsContainer.appendChild(buildingElement);
     elements.buildingUpgradesContainer.appendChild(buildingUpgradeElement);
     elements.buildingUpgrades2Container.appendChild(buildingUpgrade2Element);
 }
@@ -211,7 +203,7 @@ var score = 0;
 var totalScore = 0;
 var power = 0; // = scorePerSecond
 var powerPerClick = 0.1;
-var prestigeVar = 0;
+//var prestigeVar = 0;
 
 /*var replacer = ["score", "power", "powerPerClick"]
 
@@ -297,7 +289,7 @@ function prettify(input) {
 }
 
 function incrementClick(number) {
-    power += number * ((prestigeVar / 1000) + 1);
+    power += number/* * ((prestigeVar / 1000) + 1)*/;
 	elements.perSecond.innerText = prettify(power);
     updateGui();
 }
@@ -311,10 +303,10 @@ function increment() {
 	
 		switch (building.action.type) {
 		case "score":
-            powerTemp += building.action.perSecond * amount * ((building.upgrades.amount + 1) * (building.upgrades.upgrades.amount + 1)) * ((prestigeVar / 1000) + 1);
+            powerTemp += building.action.perSecond * amount * ((building.upgrades.amount + 1) * (building.upgrades.upgrades.amount + 1))/* * ((prestigeVar / 1000) + 1)*/;
             break;
 		case "building":
-            buildings[building.action.building].amount += building.action.perSecond * amount * delta * ((building.upgrades.amount + 1) + (building.upgrades.upgrades.amount + 1)) * ((prestigeVar / 1000) + 1);
+            buildings[building.action.building].amount += building.action.perSecond * amount * delta * ((building.upgrades.amount + 1) + (building.upgrades.upgrades.amount + 1))/* * ((prestigeVar / 1000) + 1)*/;
 			break;
 		}
 	});
@@ -361,22 +353,20 @@ function buildingUpgrade2Click(buildingUpgrade2) {
     }
 }
 
-function prestige() {
-    prestigeVar += Math.floor(totalScore / 1000000000);
-    score = 0;
-    totalScore = 0;
-    power = 0;
-    powerPerClick = 0.1;
-    setBuildings();
-    setUpgrades();
-}
+//function prestige() {
+//    prestigeVar += Math.floor(totalScore / 1000000000);
+//    score = 0;
+//    totalScore = 0;
+//    power = 0;
+//    powerPerClick = 0.1;
+//}
 
 function updateGui() {
 	elements.score.innerText = prettify(score);
 	elements.perSecond.innerText = prettify(power + getBuildingPower());
-	elements.powerPerClick.innerText = prettify(powerPerClick * ((prestigeVar / 1000) + 1));
-    elements.prestige.innerText = prettify(prestigeVar);
-    elements.prestigeNext.innerText = prettify(Math.floor(totalScore / 1000000000));
+	elements.powerPerClick.innerText = prettify(powerPerClick)/* * ((prestigeVar / 1000) + 1))*/;
+//    elements.prestige.innerText = prettify(prestigeVar);
+//    elements.prestigeNext.innerText = prettify(Math.floor(totalScore / 1000000000));
 		
 	upgrades.forEach(function (upgrade) {
 		upgrade.element.disabled = upgrade.cost > score;
@@ -423,7 +413,7 @@ window.setInterval(function () {
 //}, 60000);
 
 elements.incrementButton.addEventListener("click", function() { incrementClick(powerPerClick); });
-elements.prestigeButton.addEventListener("click", function() { prestige(); });
+//elements.prestigeButton.addEventListener("click", function() { prestige(); });
 //elements.saveButton.addEventListener("click", function() { save(); });
 //elements.deleteSaveButton.addEventListener("click", function() { deleteSave(); });
 
